61abac5af4069eddc97638e7106bcf77d57c4e57
Daniel Co <dcopythontest@gmail.com>
2020-03-17 00:51:00 -0700

    completed html, cs, and js

--- pomodoro.js
+++ pomodoro.js
@@ -1,13 +1,31 @@
 function startTimer() {
     startT = setInterval(countDownTime, 1000);
-    //disable minute up and down buttons while timer is running
+
+    //disable minute buttons while timer is running
+    minuteDown.disabled = true;
+    minuteUp.disabled = true;
+
+    //disable break buttons while timer is running
+    breakDown.disabled = true;
+    breakUp.disabled = true;
+    
     //disable play button until stop button is pressed
+    playButton.disabled = true;
 }
 
 function stopTimer() {
     clearInterval(startT);
+
     //re-enable minute buttons
+    minuteDown.disabled = false;
+    minuteUp.disabled = false;
+
+    //re-enable break buttons
+    breakDown.disabled = false;
+    breakUp.disabled = false;
+    
     //re-enable play button
+    playButton.disabled = false;
 }
 
 function resetTimer() {
@@ -17,11 +35,16 @@ function resetTimer() {
 }
 
 function calculateTime() {
+    switchTimers(); //check if timers have hit zero
+
     let minutes = Math.floor(t / 60);
     let seconds = Math.floor(t % 60);
 
-    if (seconds == 0) {
+    if (seconds == 0) { //fix formatting at 0
         seconds = "00";
+    }
+    else if (seconds < 10) { //fix formatting when at single digits
+        seconds = "0" + seconds;
     }
 
     timer.innerHTML = minutes + ":" + seconds;
@@ -30,7 +53,7 @@ function calculateTime() {
 }
 
 function countDownTime() {
-    let newTimes = calculateTime();
+    calculateTime();
     t--;
 }
 
@@ -47,12 +70,28 @@ function changeMinutes(int) {
 function changeBreak(int) {
     breakT += int;
     breakDisplay.innerHTML = Math.floor(breakT / 60); 
-    //disable break buttons when play button is pressed
+}
+
+function switchTimers() {
+    if (t == 0 && onBreak == false) {
+        clearInterval(startT);
+        t = breakT;
+        startTimer();
+        onBreak = true;
+    }
+    else if (t == 0 && onBreak == true) {
+        clearInterval(startT);
+        t = minuteDisplay.innerHTML * 60;
+        breakT = breakDisplay.innerHTML * 60;
+        startTimer();
+        onBreak = false;
+    }
 }
 
 let startT; //track timer
 let t = 1500; //25 mins default
 let breakT = 300; //5 mins default
+let onBreak = false;
 
 //controls for minutes
 minuteDisplay.innerHTML = "25";
@@ -73,11 +112,11 @@ breakDown.addEventListener('click', function() {
 });
 
 //buttons
-const playbutton = document.getElementById("play");
-playbutton.addEventListener('click', startTimer);
+const playButton = document.getElementById("play");
+playButton.addEventListener('click', startTimer);
 
-const stopbutton = document.getElementById("stop");
-stopbutton.addEventListener('click', stopTimer);
+const stopButton = document.getElementById("stop");
+stopButton.addEventListener('click', stopTimer);
 
-const resetbutton = document.getElementById("reset");
-resetbutton.addEventListener('click', resetTimer);
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener('click', resetTimer);
